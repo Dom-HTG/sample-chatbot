c5381a27c62890a622a908d02755ce94c1fed9d6
Dominic <dominicdutchboy@gmail.com>
2024-10-19 13:29:16 +0200

    to fix: ejs rendering

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
@@ -12,10 +13,20 @@ const model = client.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 const app = express();
 
+app.set('view engine', 'ejs'); //ejs to render html templates
+
+//middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public')); //serve static files from the public folder.
+
+
+app.get('/', (_, res) => {
+    res.render('index', { userPrompt: null, aiReply: null });
+})
 
 app.post('/generate-text', async (req, res) => {
-    const { prompt } = req.body;
+    const prompt = req.body.prompt;
 
     if (!prompt) {
         return res.status(400).send('BAD_REQUEST: Please input a valid prompt');
@@ -23,6 +34,8 @@ app.post('/generate-text', async (req, res) => {
 
     try {
         const response = await model.generateContent(prompt);
+        res.render('index', { userPrompt: prompt, aiReply: response.candidate.content.parts.text});
+
         return res.status(200).json({ reply: response });
 
     } catch (e) {
@@ -32,5 +45,5 @@ app.post('/generate-text', async (req, res) => {
     }
 })
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
